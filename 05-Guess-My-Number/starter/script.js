346882196fd8eb46fa581309e7ce462a5b71bfde
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-06-20 17:51:23 +0100

    Guess my number game challenge done

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,64 @@
 'use strict';
+
+// document.querySelector('.message').textContent = 'Correct number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
+// document.querySelector('.guess').value = 23;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+// document.querySelector('.number').textContent = secretNumber; // showing number for testing
+
+// Making code dry while making function for siplaying different message
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // No guess
+  if (!guess) {
+    displayMessage('No number!');
+
+    // When they win
+  } else if (guess === secretNumber) {
+    displayMessage('You WON!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    // changing CSS when winning
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+
+    // Highscore check
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // Guess is wrong
+  } else if (guess !== secretNumber) {
+    displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    displayMessage('You lost :(');
+  }
+});
+
+// Coding Challenge #1
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.score').textContent = score;
+  displayMessage('Start guessing...');
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
